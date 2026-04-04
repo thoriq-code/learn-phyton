4121c1d4f3111f311499d4488c76b86328cfa686
riiq_07 <54975037+thoriq-code@users.noreply.github.com>
2026-04-04 07:13:05 +0700

    Add files via upload

---
+++ 2. Hello World/hello-world.py
@@ -0,0 +1,4 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello World")
+Hello World
