7a47edcfab28312868afd9bf16374e55b303735b
riiq_07 <54975037+thoriq-code@users.noreply.github.com>
2026-04-05 06:37:36 +0700

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,1 +1,8 @@
+#this is my first project in phyton
+#I hope this is my first step for advanced ideas of the technology industry
+#honesly I feel enjoy for learn phyton and I'm sure that one day, by studying in depth, I can master Python
 
+#Python itself has quite unique data types when we compare it to other programming languages
+
+#boolean
+print(true)
