921a1f9ea678a410edc617ba95bdcf3aabcf520a
riiq_07 <54975037+thoriq-code@users.noreply.github.com>
2026-04-05 06:52:33 +0700

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,8 +1,43 @@
-#this is my first project in phyton
+#this is my first project in python
 #I hope this is my first step for advanced ideas of the technology industry
-#honesly I feel enjoy for learn phyton and I'm sure that one day, by studying in depth, I can master Python
+#honesly I feel enjoy for learn python and I'm sure that one day, by studying in depth, I can master Python
 
 #Python itself has quite unique data types when we compare it to other programming languages
 
 #boolean
 print(True)
+
+#string
+print("I am Learn Python")
+
+#integer
+print(20)
+
+#float
+print(3.14)
+
+#hexadecimal
+print(0x9a)
+
+#complex
+print(5j)
+
+#list
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+#tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#set
+print({1,2,3,4,5})
+print({"apel", "jeruk", "mangga"})
+
+#dictionary
+print({"nama":"budi", 'umur':20})
+
+#dictionary data type is inserted into the variable
+biodata = {"nama":"budi", 'umur':20}
+print(biodata)
+print(type(biodata))
