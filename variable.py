4b508fa57bf741fd81379026f8d107333952b016
riiq_07 <54975037+thoriq-code@users.noreply.github.com>
2026-04-09 16:37:47 +0700

    Add variable definitions and print statements

---
+++ variable.py
@@ -0,0 +1,18 @@
+#A variable is the location of the memory that is reserved for storing values. This means that when you create a variable you reserve some space in memory.
+
+Name = "Thoriq Code"
+print(Name)
+
+Language = "Python"
+print(Language)
+type(Language)
+Age = 24
+print(Age)
+type(Age)
+
+firstName = "Thoriq"
+lastName = "Code"
+Name = firstName + " " + lastName
+Age = 25
+Hobby = "Coding"
+print("Biodata\n", Name, "\n", Age, "\n", "Hobby")
